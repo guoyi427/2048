891f99b89ecae6d4643591d3c2198cdb5cbf429d
guoyi <guoyi@kokozu.net>
2017-11-21 17:40:41 +0800

    delete don't need

--- Game2048/GameViewController.swift
+++ Game2048/GameViewController.swift
@@ -12,44 +12,55 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    fileprivate let _scene = SKScene(size: UIScreen.main.bounds.size)
+    
+    fileprivate var _nodel: SKNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            
+            prepareScene(view: view)
+            prepareChildNode()
             
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
     }
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+}
+
+// MARK: - Prepare
+extension GameViewController {
+    fileprivate func prepareScene(view: SKView) {
+        _scene.scaleMode = .aspectFill
+        view.presentScene(_scene)
+        
+        let swipeGR = UISwipeGestureRecognizer(target: self, action: #selector(swipeViewGestureRecognizerAction))
+        view.addGestureRecognizer(swipeGR)
+    }
+    
+    fileprivate func prepareChildNode() {
+        
+        let node = SKShapeNode(rectOf: CGSize(width: 10, height: 10), cornerRadius: 3)
+        node.position = view.center
+        node.fillColor = UIColor.red
+        _scene.addChild(node)
+        _nodel = node
+    }
+}
+
+// MARK: - GestureRecognizer - Action
+extension GameViewController {
+    @objc
+    fileprivate func swipeViewGestureRecognizerAction() {
+        if let node = _nodel {
+            node.run(SKAction.move(to: CGPoint(x: 200, y: 200), duration: 0.25))
+        }
     }
 }
