41c299f7a3c3d0b9890283cc50d620f38c5259f6
guoyi <guoyi@kokozu.net>
2017-11-21 16:58:32 +0800

    first

---
+++ Game2048Tests/Game2048Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Game2048Tests.swift
+//  Game2048Tests
+//
+//  Created by kokozu on 21/11/2017.
+//  Copyright © 2017 guoyi. All rights reserved.
+//
+
+import XCTest
+@testable import Game2048
+
+class Game2048Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
