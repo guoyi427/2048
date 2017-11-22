f2f64d2f05725e48b4bd7a51d64eef58f3e46912
guoyi <guoyi@kokozu.net>
2017-11-22 23:35:08 +0800

    add swipe right, down and up

--- Game2048/GameViewController.swift
+++ Game2048/GameViewController.swift
@@ -86,10 +86,16 @@ extension GameViewController {
 
         node.run(SKAction.move(to: point, duration: 0.25)) {
             //  移除之前在这里的node
-            let lastNode = self._cellNodeList[column][row]
-            lastNode.removeFromParent()
-            self._cellNodeList[column][row] = node
+//            let lastNode = self._cellNodeList[column][row]
+//            lastNode.removeFromParent()
+//            self._cellNodeList[column][row] = node
         }
+        removeCellNodel(column: column, row: row, newNode: node)
+    }
+    
+    fileprivate func removeCellNodel(column: Int, row: Int, newNode: CellNode) {
+        _cellNodeList[column][row].removeFromParent()
+        _cellNodeList[column][row] = newNode
     }
     
     /// 根据model 添加一个cellNode
@@ -115,9 +121,10 @@ extension GameViewController {
 extension GameViewController {
     @objc
     fileprivate func swipeViewGestureRecognizerAction(sender: UISwipeGestureRecognizer) {
+        var doSomething: Bool = false
+        
         switch sender.direction {
         case .left:
-            var doSomething: Bool = false
             for column in 0...DataManager.shared.currentModelList.count-1 {
                 let modelList = DataManager.shared.currentModelList[column]
                 /// 上一个 不为空的 number
@@ -147,11 +154,12 @@ extension GameViewController {
                             _cellNodeList[column][row] = CellNode(number: 0, size: CGSize.zero)
                             
                             moveCellNode(column: column, row: lastIndex+1, node: node)
+                            _cellNodeList[column][lastIndex+1].updateNumber(number: model.number)
                             
                             DataManager.shared.currentModelList[column][lastIndex+1].number = model.number
                             model.number = 0
                             
-                            lastNumber = DataManager.shared.currentModelList[column][row].number
+                            lastNumber = DataManager.shared.currentModelList[column][lastIndex+1].number
                             lastIndex += 1
                             doSomething = true
                         } else {
@@ -162,18 +170,156 @@ extension GameViewController {
                     }
                 }
             }
-//            if doSomething {
-                addCellNode(model: DataManager.shared.addCellModel())
-//            }
             break
+            
         case .right:
+            for column in 0...DataManager.shared.currentModelList.count-1 {
+                let modelList = DataManager.shared.currentModelList[column]
+                /// 上一个 不为空的 number
+                var lastNumber: Int = 0
+                /// 上一个 不为空的 下标
+                var lastIndex: Int = modelList.count
+                for i in 0...modelList.count-1 {
+                    let row = modelList.count - i - 1
+                    let model = modelList[row]
+                    if model.number != 0 {
+                        if model.number != 0 && lastNumber == model.number {
+                            //  相同的数字 合并
+                            let currentNumber = model.number + 1
+                            model.number = 0    //  还原当前number
+                            let node = _cellNodeList[column][row]
+                            _cellNodeList[column][row] = CellNode(number: 0, size: CGSize.zero)
+                            
+                            moveCellNode(column: column, row: lastIndex, node: node)
+                            node.updateNumber(number: currentNumber)
+                            
+                            DataManager.shared.currentModelList[column][lastIndex].number = currentNumber
+                            
+                            lastNumber = model.number
+                            doSomething = true
+                        } else if lastIndex > row + 1 {
+                            //  数字不相同 并且需要想右挪
+                            let node = _cellNodeList[column][row]
+                            _cellNodeList[column][row] = CellNode(number: 0, size: CGSize.zero)
+                            
+                            moveCellNode(column: column, row: lastIndex-1, node: node)
+                            _cellNodeList[column][lastIndex-1].updateNumber(number: model.number)
+                            
+                            DataManager.shared.currentModelList[column][lastIndex-1].number = model.number
+                            model.number = 0
+                            
+                            lastNumber = DataManager.shared.currentModelList[column][lastIndex-1].number
+                            lastIndex -= 1
+                            doSomething = true
+                        } else {
+                            //  数字不相同，也不需要想左挪
+                            lastNumber = model.number
+                            lastIndex = row
+                        }
+                    }
+                }
+            }
             break
+            
         case .down:
+            for row in 0...DataManager.shared.currentModelList.first!.count-1 {
+                /// 上一个 不为空的 number
+                var lastNumber: Int = 0
+                /// 上一个 不为空的 下标
+                var lastIndex: Int = -1
+                for column in 0...DataManager.shared.currentModelList.count-1 {
+                    let model = DataManager.shared.currentModelList[column][row]
+                    if model.number != 0 {
+                        if model.number != 0 && lastNumber == model.number {
+                            //  相同的数字 合并
+                            let currentNumber = model.number + 1
+                            model.number = 0    //  还原当前number
+                            let node = _cellNodeList[column][row]
+                            _cellNodeList[column][row] = CellNode(number: 0, size: CGSize.zero)
+                            
+                            moveCellNode(column: lastIndex, row: row, node: node)
+                            node.updateNumber(number: currentNumber)
+                            
+                            DataManager.shared.currentModelList[lastIndex][row].number = currentNumber
+                            
+                            lastNumber = model.number
+                            doSomething = true
+                        } else if lastIndex < column - 1 {
+                            //  数字不相同 并且需要想下挪
+                            let node = _cellNodeList[column][row]
+                            _cellNodeList[column][row] = CellNode(number: 0, size: CGSize.zero)
+                            
+                            moveCellNode(column: lastIndex+1, row: row, node: node)
+                            _cellNodeList[lastIndex+1][row].updateNumber(number: model.number)
+                            
+                            DataManager.shared.currentModelList[lastIndex+1][row].number = model.number
+                            model.number = 0
+                            
+                            lastNumber = DataManager.shared.currentModelList[lastIndex+1][row].number
+                            lastIndex += 1
+                            doSomething = true
+                        } else {
+                            //  数字不相同，也不需要想左挪
+                            lastNumber = model.number
+                            lastIndex = column
+                        }
+                    }
+                }
+            }
             break
         case .up:
+            for row in 0...DataManager.shared.currentModelList.first!.count-1 {
+                /// 上一个 不为空的 number
+                var lastNumber: Int = 0
+                /// 上一个 不为空的 下标
+                var lastIndex: Int = DataManager.shared.currentModelList.count
+                for i in 0...DataManager.shared.currentModelList.count-1 {
+                    let column = DataManager.shared.currentModelList.count - i - 1
+                    let model = DataManager.shared.currentModelList[column][row]
+                    if model.number != 0 {
+                        if model.number != 0 && lastNumber == model.number {
+                            //  相同的数字 合并
+                            let currentNumber = model.number + 1
+                            model.number = 0    //  还原当前number
+                            let node = _cellNodeList[column][row]
+                            _cellNodeList[column][row] = CellNode(number: 0, size: CGSize.zero)
+                            
+                            moveCellNode(column: lastIndex, row: row, node: node)
+                            node.updateNumber(number: currentNumber)
+                            
+                            DataManager.shared.currentModelList[lastIndex][row].number = currentNumber
+                            
+                            lastNumber = model.number
+                            doSomething = true
+                        } else if lastIndex > column + 1 {
+                            //  数字不相同 并且需要想上挪
+                            let node = _cellNodeList[column][row]
+                            _cellNodeList[column][row] = CellNode(number: 0, size: CGSize.zero)
+                            
+                            moveCellNode(column: lastIndex-1, row: row, node: node)
+                            _cellNodeList[lastIndex-1][row].updateNumber(number: model.number)
+                            
+                            DataManager.shared.currentModelList[lastIndex-1][row].number = model.number
+                            model.number = 0
+                            
+                            lastNumber = DataManager.shared.currentModelList[lastIndex-1][row].number
+                            lastIndex -= 1
+                            doSomething = true
+                        } else {
+                            //  数字不相同，也不需要想左挪
+                            lastNumber = model.number
+                            lastIndex = column
+                        }
+                    }
+                }
+            }
             break
         default:
             break
+        }
+        
+        if doSomething {
+            addCellNode(model: DataManager.shared.addCellModel())
         }
     }
 }
