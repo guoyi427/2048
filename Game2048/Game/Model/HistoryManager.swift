1c57f7067332f9a1e343b020eb08ffa12cd4b5a6
Gray <13311300131@189.cn>
2018-06-12 14:30:31 +0800

    添加数据库第三方

---
+++ Game2048/Game/Model/HistoryManager.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryManager.swift
+//  Game2048
+//
+//  Created by 默认 on 2018/6/12.
+//  Copyright © 2018年 guoyi. All rights reserved.
+//
+
+import UIKit
+
+
+class HistoryManager: NSObject {
+
+    static let shared = HistoryManager()
+    
+    
+    override init() {
+        
+    }
+    
+}
--- Podfile
+++ Podfile
@@ -3,4 +3,5 @@ platform :ios, '8.0'
 
 target 'Game2048' do
     pod 'Google-Mobile-Ads-SDK'
+    pod 'SQLite.swift'
 end
